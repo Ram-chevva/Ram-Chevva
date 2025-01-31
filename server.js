48107de14d365353a87ea136e3d440bfa88b4729
Svelpula1202 <saisruthivelpula@gmail.com>
2025-01-31 16:49:27 -0500

    Add server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8080;
+const port = 5500;
 
 // Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
